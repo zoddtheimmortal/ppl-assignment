0922830addc92819791bf5e87a7cbf6c726511ee
Alok Desai <alokdesai2004@gmail.com>
2024-11-20 06:11:14 +0530

    chore: add rq1 template

---
+++ function/dfs.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (print-dfs-result graph start-node)
+  ;; write logic here
+
+(define (main)
+  (define graph (read))
+  (define start-node (read))
+  (print-dfs-result graph start-node))
+
+(main)
