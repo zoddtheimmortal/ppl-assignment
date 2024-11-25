7642bb82b82b60f541668f08d83b604b766e82fc
Alok Desai <alokdesai2004@gmail.com>
2024-11-25 05:44:56 +0530

    feat: add q2 dfs

--- function/dfs.rkt
+++ function/dfs.rkt
@@ -1,7 +1,25 @@
 #lang racket
 
+(define visited '())
+
+(define (dfs graph node)
+  (if (member node visited)
+      visited
+      (let* ((entry (assoc node graph))
+         (neighbors (if entry (second entry) '()))
+         (new-visited (append (list node) visited)))
+        (set! visited new-visited)
+        (for-each (lambda (neighbor)
+              (dfs graph neighbor))
+            neighbors)
+        visited)))
+
 (define (print-dfs-result graph start-node)
-  ;; write logic here
+    (set! visited '())
+    (dfs graph start-node)
+    (set! visited (reverse visited))
+    (display visited)
+    (newline))
 
 (define (main)
   (define graph (read))
